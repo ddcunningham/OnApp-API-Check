1c570f1a9c61e5f16df47ae2a8590cb9109620c3
David <daviddeleoncunningham@gmail.com>
2016-02-10 20:29:51 -0700

    first working prototype

--- jsontest.php
+++ jsontest.php
@@ -1,11 +1,42 @@
-<?php
+<?php 
 date_default_timezone_set("UTC");
-#$today = getdate();
-#$datestring = date("Y-m-d+H:00:00",$today[0]);
+$today = getdate();
+$datestring = date("Y-m-d+H:00:00",$today[0]);
 
+/*** DOWNLOAD FILE FROM CURL ****/
 
+//API KEY AND PASSWORD
+$username='admin';
+$password='hf#-RZFew(78{K7vp~MHZ';
+$usernamePassword = $username . ':' . $password;
+$headers = array('Authorization: Basic ' . base64_encode($usernamePassword), 'Content-Type: application/json');
 
-$json = file_get_contents("http://23.239.210.199/virtual_machines/frlmso3lgct6ag/network_interfaces/379/usage.json?period[startdate]=2016-02-03+19:00:00&period[enddate]=2016-02-04+21:00:00");
+//URL
+$url= "http://onappcp.accuwebhosting.com/virtual_machines/frlmso3lgct6ag/network_interfaces/379/usage.json?period[startdate]=$datestring&period[enddate]=$datestring";
+
+//FILE NAME
+$filename = 'bandwidth_api_call.json';
+
+//DOWNLOAD PATH
+$path = '/tmp/'.$filename;
+
+//FOLDER PATH
+$fp = fopen($path, 'w');
+
+//SETTING UP CURL REQUEST
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+
+//CONNECTION CLOSE
+curl_close($ch);
+fclose($fp);
+
+echo $url;
+
+$json = file_get_contents($path);
 $jvar = json_decode($json,TRUE);
 print_r($jvar);
 
